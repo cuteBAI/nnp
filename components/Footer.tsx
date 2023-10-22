e81448495436f0d4bb7d0737a0e1b543567b500b
cuteBAI <111678489+cuteBAI@users.noreply.github.com>
2023-10-22 12:03:55 +0800

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -36,7 +36,8 @@ export const FooterImpl: React.FC = () => {
   return (
     <footer className={styles.footer}>
       <div className={styles.copyright}>Copyright 2023 {config.author}</div>
-
+        <a href="https://icp.gov.moe/?keyword=20230480" target="_blank">萌ICP备20230480号</a>
+      
       <div className={styles.settings}>
         {hasMounted && (
           <a
